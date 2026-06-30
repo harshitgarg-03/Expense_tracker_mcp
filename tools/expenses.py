13497718cc5e7821a2530f2719a80fdd2cdb2084
Harshit Garg~ <gargh2120@gmail.com>
2026-06-30 15:06:43 +0530

    fix changes

--- tools/expenses.py
+++ tools/expenses.py
@@ -4,20 +4,22 @@ from utils import get_api
 
 mcp = FastMCP()
 
-api = get_api()
-
 @mcp.tool()
 async def list_expenses():
+    api = get_api()
     return await api.get_expense()
 
 @mcp.tool()
 async def add_expenses():
+    api = get_api()
     return await api.add_expense()
 
 @mcp.tool()
 async def edit_expenses():
+    api = get_api()
     return await api.edit_expense()
 
 @mcp.tool()
 async def delete_expenses():
+    api = get_api()
     return await api.delete_expense()
