39180f7e3a72a1b16b5aefa1314f40f3afb94653
Harshit Garg~ <gargh2120@gmail.com>
2026-06-30 14:20:25 +0530

    changes with api_client

--- tools/expenses.py
+++ tools/expenses.py
@@ -1,25 +1,23 @@
 from fastmcp import FastMCP
 
-from api_client import ExpenseApi
+from utils import get_api
 
 mcp = FastMCP()
 
+api = get_api()
+
 @mcp.tool()
 async def list_expenses(token: str):
-    api = ExpenseApi(token)
     return await api.get_expense()
 
 @mcp.tool()
 async def add_expenses(token: str):
-    api = ExpenseApi(token)
     return await api.add_expense()
 
 @mcp.tool()
 async def edit_expenses(token: str):
-    api = ExpenseApi(token)
     return await api.edit_expense()
 
 @mcp.tool()
 async def delete_expenses(token: str):
-    api = ExpenseApi(token)
     return await api.delete_expense()
