1a28881b15a97162d2d92486a430dda0181f1f93
Harshit Garg~ <gargh2120@gmail.com>
2026-07-02 17:58:12 +0530

    data adding or passing

--- tools/expenses.py
+++ tools/expenses.py
@@ -2,6 +2,8 @@ from fastmcp import FastMCP
 
 from utils import get_api
 
+from schemas.expense import Expense_Create
+
 mcp = FastMCP()
 
 @mcp.tool()
@@ -10,16 +12,16 @@ async def list_expenses():
     return await api.get_expense()
 
 @mcp.tool()
-async def add_expenses():
+async def add_expenses(expense: Expense_Create):
     api = get_api()
-    return await api.add_expense()
+    return await api.add_expense(expense.model_dump(mode="json"))
 
 @mcp.tool()
-async def edit_expenses():
+async def edit_expenses(id: str, expense: Expense_Create):
     api = get_api()
-    return await api.edit_expense()
+    return await api.edit_expense(id, expense.model_dump(mode="json"))
 
 @mcp.tool()
-async def delete_expenses():
+async def delete_expenses(id: str):
     api = get_api()
-    return await api.delete_expense()
+    return await api.delete_expense(id)
