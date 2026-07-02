902a3e41f13b0c001f80d9c2ff7548f9d7cfe6d6
Harshit Garg~ <gargh2120@gmail.com>
2026-07-02 17:51:32 +0530

    adding schema defined

---
+++ schemas/expense.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+
+from datetime import date
+
+from typing import Literal
+
+class Expense_Create(BaseModel):
+
+    title: str = Field(..., min_length=1)
+    amount: Decimal = Field(..., gt = 0, description="Transaction amount")
+    date: datetime = Field(default_factory=datetime.now())
+    category: Literal[  "Food",
+                        "Travel",
+                        "Bills",
+                        "Entertainment",
+                        "Shopping",
+                        "Health",
+                        "Salary",
+                        "Freelance",
+                        "Other"]
+    
+    type: Literal["income", "expense"]
+    note: str | None = None
+
+
+
