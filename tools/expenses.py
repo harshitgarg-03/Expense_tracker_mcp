71a23ccfd90e0c88c2ee5bed75a31b72e2f95999
Harshit Garg~ <gargh2120@gmail.com>
2026-07-07 22:20:27 +0530

    adding json and returning in dumps

--- tools/expenses.py
+++ tools/expenses.py
@@ -2,13 +2,16 @@ from mcp_server import mcp
 from tools.utils import get_api
 from schemas.expense import ExpenseCreate
 
+import json
+
 @mcp.tool(
     name="list_expenses",
     description="Retrieve a list of all transactions/expenses for the logged-in user."
 )
 async def list_expenses():
     api = get_api()
-    return await api.get_expense()
+    result = await api.get_expense()
+    return json.dumps(result)
 
 @mcp.tool(
     name="add_expense",
@@ -17,14 +20,7 @@ async def list_expenses():
 async def add_expense(expense: ExpenseCreate):
     api = get_api()
     result = await api.add_expense(expense)
-    return (
-        f"Expense added successfully.\n"
-        f"ID: {result['id']}\n"
-        f"Title: {result['title']}\n"
-        f"Amount: ₹{result['amount']}\n"
-        f"Category: {result['category']}\n"
-        f"Type: {result['type']}"
-    )
+    return json.dumps( result );
 
 @mcp.tool(
     name="edit_expense",
@@ -32,7 +28,8 @@ async def add_expense(expense: ExpenseCreate):
 )
 async def edit_expense(id: str, expense: ExpenseCreate):
     api = get_api()
-    return await api.edit_expense(id, expense)
+    res = await api.edit_expense(id, expense)
+    return json.dumps(res)
 
 @mcp.tool(
     name="delete_expense",
@@ -40,4 +37,5 @@ async def edit_expense(id: str, expense: ExpenseCreate):
 )
 async def delete_expense(id: str):
     api = get_api()
-    return await api.delete_expense(id)
+    res = await api.delete_expense(id)
+    return json.dumps(res)
