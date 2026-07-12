6d28efec903a3287d90d6ab7736d221ace49adf8
Harshit Garg~ <gargh2120@gmail.com>
2026-07-12 22:20:33 +0530

    adding urls

--- config.py
+++ config.py
@@ -7,4 +7,6 @@ DATABASE_URI = os.getenv("DATABASE_URI")
 
 BETTER_AUTH_SECRET = os.getenv("BETTER_AUTH_SECRET")
 
+MCP_RESOURCE_URI = os.environ["MCP_RESOURCE_URI"]  
+
 API_BASE_URL = os.getenv("API_BASE_URL")
--- session.py
+++ session.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-SESSION_FILE = Path("session.json")
+# SESSION_FILE = Path("session.json")
 
 
 # mcp session token stored here ..
@@ -12,20 +12,20 @@ class SessionStore :
         self.cookie_name = None
         self.user = None
 
-    def save(self):
-        SESSION_FILE.write_text(json.dumps({
-            "token": self.token,
-            "cookie_name": self.cookie_name,
-            "user": self.user,
-        }))
+    # def save(self):
+    #     SESSION_FILE.write_text(json.dumps({
+    #         "token": self.token,
+    #         "cookie_name": self.cookie_name,
+    #         "user": self.user,
+    #     }))
 
-    def load(self):
-        if SESSION_FILE.exists():
-            data = json.loads(SESSION_FILE.read_text())
+    # def load(self):
+    #     if SESSION_FILE.exists():
+    #         data = json.loads(SESSION_FILE.read_text())
 
-            self.token = data.get("token")
-            self.cookie_name = data.get("cookie_name")
-            self.user = data.get("user")
+    #         self.token = data.get("token")
+    #         self.cookie_name = data.get("cookie_name")
+    #         self.user = data.get("user")
 
         
 session = SessionStore()
